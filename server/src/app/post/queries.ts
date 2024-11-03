e71c8b57f1ec9fffa888abb9c00509fdac9b8b81
Vineet829 <vineetsingh5987@gmail.com>
2024-11-04 03:45:46 +0530

    feat: add getTotalLikesForPost query

--- server/src/app/post/queries.ts
+++ server/src/app/post/queries.ts
@@ -1,7 +1,8 @@
+
 export const queries = `#graphql
     getAllPosts: [Post]
     getSignedURLForPost(imageName: String!, imageType: String!): String
     getPostById(id: ID!): Post
     getAllComments(postId: ID!): [Comment]
-    getTotalLikesForPost(postId: ID!): Int
+    getTotalLikesForPost(postId: ID!): PostLikes!
 `;
