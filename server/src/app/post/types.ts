552402890b0cd28c0d103bc108b231316a037c34
Vineet829 <vineetsingh5987@gmail.com>
2024-11-04 03:44:44 +0530

    feat: add Post type definition

--- server/src/app/post/types.ts
+++ server/src/app/post/types.ts
@@ -1,34 +1,39 @@
-
 export const types = `#graphql
 
     input CreatePostData {
         content: String!
         imageURL: String
     }
-    input CreateCommentData {
-  content: String!
-  
-  postId: ID!
-}
 
- 
-   
-    type Comment{
+    input CreateCommentData {
+        content: String!
+        postId: ID!
+    }
+
+    type PostLikes {
+        totalCount: Int!
+        likes: [Like]!
+    }
+
+    type Comment {
         id: ID!
         content: String!
         user: User
-        
+        post: Post
     }
-    
-    
+
+    type Like {
+        user: User
+        post: Post
+        createdAt: String
+    }
+
     type Post {
         id: ID!
         content: String!
         imageURL: String
-        likes: [User]   
+        likes: [Like]   
         author: User
-        comments: [User]
-        
-        
+        comments: [Comment] 
     }
 `;
