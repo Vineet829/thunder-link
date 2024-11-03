3543421637198d986932bce4e36398e030a32194
Vineet829 <vineetsingh5987@gmail.com>
2024-11-04 03:46:15 +0530

    feat: add getTotalLikesForPost query

--- server/src/app/post/resolvers.ts
+++ server/src/app/post/resolvers.ts
@@ -5,7 +5,8 @@ import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 import { prismaClient } from "../../clients/db";
 import { GraphqlContext } from "../../intefaces";
 import UserService from "../../services/user";
-import PostService, { CreatePostPayload, CreateCommentData } from "../../services/post";
+import PostService, { CreatePostPayload, CreateCommentData} from "../../services/post";
+
 
 const s3Client = new S3Client({
   region: process.env.AWS_DEFAULT_REGION,
@@ -13,7 +14,6 @@ const s3Client = new S3Client({
 
 const queries = {
   getAllPosts: () => PostService.getAllPosts(),
-  
   getSignedURLForPost: async (
     parent: any,
     { imageType, imageName }: { imageType: string; imageName: string },
@@ -37,34 +37,43 @@ const queries = {
     });
 
     const signedURL = await getSignedUrl(s3Client, putObjectCommand);
+
     return signedURL;
   },
-  
   getPostById: async (
     parent: any,
     { id }: { id: string },
     ctx: GraphqlContext
-  ) => PostService.getPostById(id),
+  ) => PostService.getPostById(id)
+,
 
-  getAllComments: (
-    parent: any,
-    { postId }: { postId: string },
-    ctx: GraphqlContext
-  ) => {
-    if (!ctx.user) {
-      throw new Error("User not authenticated");
-    }
+getAllComments: (
+  parent: any, 
+  { postId }: { postId: string },
+  ctx: GraphqlContext 
+) => {
+ 
+  if (!ctx.user) {
+    
+    throw new Error("User not authenticated");
+  }
 
     const comments = PostService.getAllComments(postId);
     return comments;
-  },
+  }  , 
 
   getTotalLikesForPost: async (
     parent: any,
     { postId }: { postId: string },
     ctx: GraphqlContext
-  ) => PostService.getTotalLikesForPost(postId),
-};
+) => {
+    const likes = await PostService.getTotalLikesForPost(postId);
+    return {
+        totalCount: likes.length,
+        likes,
+    };
+}
+}
 
 const mutations = {
   createPost: async (
@@ -79,121 +88,170 @@ const mutations = {
     });
 
     return post;
-  },
+  }
+,
 
-  addCommentToPost: async (
-    parent: any,
-    { payload }: { payload: CreateCommentData },
-    ctx: GraphqlContext
-  ) => {
-    if (!ctx.user) throw new Error("You are not authenticated");
+addCommentToPost: async (
+  parent: any,
+  { payload }: { payload: CreateCommentData },
+  ctx: GraphqlContext
+) => {
+ 
+  if (!ctx.user) throw new Error("You are not authenticated");
+  
+  
+  const comment = await PostService.addCommentToPost({
+    content: payload.content,
+    postId: payload.postId,
+    userId: ctx.user.id 
+  });
 
-    const comment = await PostService.addCommentToPost({
-      content: payload.content,
-      postId: payload.postId,
-      userId: ctx.user.id
-    });
+  
+  return comment;
+}
 
-    return comment;
-  },
+,  
 
-  likePost: async (
-    parent: any,
-    { postId }: { postId: string },
+
+
+likePost: async (
+    parent:any,
+    { postId} : {postId: string}, 
     ctx: GraphqlContext
   ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("unauthenticated");
 
+    
     await PostService.likePost(ctx.user.id, postId);
-    return true;
+
+    
+   
+
+    return true; 
   },
+  
 
   unlikePost: async (
-    parent: any,
-    { postId }: { postId: string },
+    parent:any,
+    { postId }: {postId: string},
     ctx: GraphqlContext
   ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("unauthenticated");
 
+   
     await PostService.unlikePost(ctx.user.id, postId);
-    return true;
-  },
 
+    
+   
+
+    return true; 
+  },
   deleteSingleComment: async (
-    parent: any,
-    { postId, commentId }: { postId: string, commentId: string },
+    parent:any,
+    { postId,commentId}: { postId: string,commentId: string }, 
     ctx: GraphqlContext
   ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("unauthenticated");
 
-    await PostService.deleteSingleComment(postId, commentId);
-    return true;
-  },
+    
+   await PostService.deleteSingleComment(postId,commentId);
 
-  userHasLikedPost: async (
-    parent: any,
+    
+   
+
+    return true; 
+  },
+  userHasLikedPost: (
+    parent: any, 
     { postId }: { postId: string },
-    ctx: GraphqlContext
+    ctx: GraphqlContext 
   ) => {
+ 
     if (!ctx.user) {
+    
       throw new Error("User not authenticated");
     }
-
-    const hasLiked = await PostService.userHasLikedPost(ctx.user.id, postId);
-    return hasLiked;
-  },
+  
+      const hasLiked = PostService.userHasLikedPost(ctx.user.id, postId);
+      return hasLiked;
+    }, 
+  
 
   deleteLikes: async (
-    parent: any,
-    { postId }: { postId: string },
+    parent:any,
+    { postId }: {postId: string}, 
     ctx: GraphqlContext
   ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("unauthenticated");
 
+   
     await PostService.deleteLikes(postId);
-    return true;
+
+   
+   
+
+    return true; 
   },
 
   deleteComments: async (
-    parent: any,
-    { postId }: { postId: string },
+    parent:any,
+    { postId }: {postId: string}, 
     ctx: GraphqlContext
   ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("unauthenticated");
 
+
     await PostService.deleteComments(postId);
-    return true;
+
+  
+   
+
+    return true; //
   },
 
   deletePost: async (
-    parent: any,
-    { postId }: { postId: string },
+    parent:any,
+    { postId }: {postId: string}, 
     ctx: GraphqlContext
   ) => {
     if (!ctx.user || !ctx.user.id) throw new Error("unauthenticated");
 
     await PostService.deletePost(ctx.user.id, postId);
-    return true;
+
+
+   
+
+    return true; 
   },
 };
 
+    
+
+
+
+
+
 const extraResolvers = {
+  
   Comment: {
-    user: (parent: Comment) => UserService.getUserById(parent.userId),
-  },
+  user: (parent: Comment) => UserService.getUserById(parent.userId),
+
+  }
+  
+  ,
   Post: {
     author: (parent: Post) => UserService.getUserById(parent.authorId),
-
-    likes: async (parent: Post) => {
-      const result = await prismaClient.like.findMany({
-        where: { post: { id: parent.id } },
-        include: {
-          user: true,
-        },
-      });
-      return result.map((el) => el.user);
-    }
-  }
+      
+  
+  likes: async (parent: Post) => {
+    const result = await prismaClient.like.findMany({
+      where: { post: { id: parent.id } },
+      include: {
+        user: true,
+      },
+    });
+    return result.map((el) => el.user);
+  }}
 };
 
 export const resolvers = { mutations, extraResolvers, queries };
