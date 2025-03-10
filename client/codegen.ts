79dd2526e71c86053481312ae341e70f6a3b3e95
Vineet Singh <78719701+Vineet829@users.noreply.github.com>
2025-03-11 02:23:50 +0530

    Update codegen.ts

--- client/codegen.ts
+++ client/codegen.ts
@@ -3,7 +3,7 @@ import type { CodegenConfig } from '@graphql-codegen/cli';
 
 const config: CodegenConfig = {
   overwrite: true,
-  schema: "https://thunder-link.onrender.com/graphql",
+  schema: "https://thunder.vineetpersonal.site/graphql",
   documents: "**/*.{tsx,ts}",
   generates: {
     "gql/": {
